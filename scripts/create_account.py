008739e5e4efd0ff52f81856a93cb096bea4d554
Anthony del Pino <anthonyjdelpino@gmail.com>
2025-07-22 12:21:19 -0400

    Checks to make sure username contains only alphanumeric characters. If not, will prompt the user to enter a new username until a valid username is entered.

--- scripts/create_account.py
+++ scripts/create_account.py
@@ -1,7 +1,14 @@
 import getpass
 import hashlib
 
-username = input('Enter username for new account: ')
+print('Your username must contain only letters and numbers.\n')
+
+while True:
+    username = input('Enter username for new account: ')
+    if username.isalnum():
+        break
+    else:
+        print('\nYour username contains invalid characters. Please try again, using only letters and numbers.\n')
 
 print('\nYour password will never be stored or sent as plaintext. After this script finishes, the plaintext will no longer be in memory either\n')
 
@@ -14,4 +21,3 @@ password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
 with open('.users', 'a') as f:
     f.write('['+username+','+password_hash+']\n')
     f.close()
-
